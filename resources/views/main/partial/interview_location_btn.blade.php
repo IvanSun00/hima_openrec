96270bd5d4df588850b5ca054c02339b91c17e21
elizabethceline <elizabethceline04@gmail.com>
2024-07-08 20:58:59 +0700

    feat: line pwwc

--- resources/views/main/partial/interview_location_btn.blade.php
+++ resources/views/main/partial/interview_location_btn.blade.php
@@ -2,5 +2,5 @@
     class="inline-block rounded w-full mb-4 bg-[#F8A348] px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white transition duration-150 ease-in-out hover:bg-[#ba7d68] focus:bg-[#ba7d68] focus:outline-none focus:ring-0 active:bg-primary-700"
     id="modalLocationBtn{{ $i }}" data-te-toggle="modal" data-te-target="#modalLocation{{ $i }}"
     data-te-ripple-init data-te-ripple-color="light">
-    Lokasi
+    Link Wawancara
 </button>
